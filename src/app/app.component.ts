fca9dd72533ccd781d28da96e9a27b9c332520ec
fusho-takahashi <zenryaku@fusho-takahashi.me>
2024-06-29 08:34:17 +0900

    テキストや画像を投げて console に結果を出力する

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,107 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  GenerateContentRequest,
+  GoogleGenerativeAI,
+  HarmBlockThreshold,
+  HarmCategory,
+  ModelParams,
+  RequestOptions,
+} from '@google/generative-ai';
+import { environment } from '../environments/environment.development';
+import { FileConversionService } from './file-conversion.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'google-ai-gemini-angular';
+export class AppComponent implements OnInit {
+  private readonly fileConversionService = inject(FileConversionService);
+
+  ngOnInit(): void {
+    // 試したい方を uncomment
+    // this.TestGeminiPro();
+    // this.TestGeminiProVisionImages();
+  }
+
+  async TestGeminiPro() {
+    const genAI = new GoogleGenerativeAI(environment.API_KEY);
+    const generationConfig: ModelParams = {
+      model: 'gemini-pro',
+      safetySettings: [
+        {
+          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+          threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+        },
+      ],
+      generationConfig: {
+        temperature: 0.9,
+        topP: 1,
+        topK: 32,
+        maxOutputTokens: 100,
+      },
+    };
+
+    const model = genAI.getGenerativeModel(generationConfig);
+
+    const prompt = 'What is the largest number with a name ?';
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    console.log(response.text());
+  }
+
+  async TestGeminiProVisionImages() {
+    const genAI = new GoogleGenerativeAI(environment.API_KEY);
+    const generationConfig: ModelParams = {
+      model: 'gemini-pro-vision',
+      safetySettings: [
+        {
+          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+          threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+        },
+      ],
+      generationConfig: {
+        temperature: 0.9,
+        topP: 1,
+        topK: 32,
+        maxOutputTokens: 100,
+      },
+    };
+
+    const model = genAI.getGenerativeModel(generationConfig);
+
+    try {
+      let imageBase64 = await this.fileConversionService.convertToBase64(
+        'assets/angular_gradient.png'
+      );
+
+      if (typeof imageBase64 !== 'string') {
+        throw new Error('Image conversion to Base64 failed.');
+      }
+
+      const prompt: GenerateContentRequest = {
+        contents: [
+          {
+            role: 'USER',
+            parts: [
+              {
+                inlineData: {
+                  mimeType: 'image/png',
+                  data: imageBase64,
+                },
+              },
+              { text: 'What logo is this?' },
+            ],
+          },
+        ],
+      };
+
+      const result = await model.generateContent(prompt);
+      const response = await result.response;
+      console.log(response.text());
+    } catch (e) {}
+  }
 }
